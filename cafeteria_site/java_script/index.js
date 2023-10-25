43d2b7bfd7b74b668939473979d4a5dcaf77884d
guicruz12 <130380751+guicruz12@users.noreply.github.com>
2023-10-25 12:31:37 -0300

    td feito hj

---
+++ cafeteria_site/java_script/index.js
@@ -0,0 +1,7 @@
+function mudarImagem() {
+    document.getElementById('promocao1').src = 'imagens/brisafe_promo_2.png';
+}
+
+function voltarImagem() {
+    document.getElementById('promocao1').src = 'imagens/promoçao.png';
+}
